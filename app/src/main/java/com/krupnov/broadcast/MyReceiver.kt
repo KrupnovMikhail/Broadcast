4763859e0bfcfe65f6a20143d0e5de9c1e122276
Mikhail Krupnov <krupnovmn@gmail.com>
2023-11-26 00:34:43 +0300

    lesson 2 and 3

--- app/src/main/java/com/krupnov/broadcast/MyReceiver.kt
+++ app/src/main/java/com/krupnov/broadcast/MyReceiver.kt
@@ -9,9 +9,27 @@ class MyReceiver: BroadcastReceiver() {
 
     override fun onReceive(p0: Context?, p1: Intent?) {
         when(p1?.action) {
+            ACTION_CLICKED -> {
+                val count = p1.getIntExtra(EXTRA_COUNT, 0)
+                Toast.makeText( p0, "Clicked $count", Toast.LENGTH_SHORT).show()
+            }
             Intent.ACTION_BATTERY_LOW -> {
                 Toast.makeText( p0, "Battery low", Toast.LENGTH_SHORT).show()
             }
+            Intent.ACTION_AIRPLANE_MODE_CHANGED -> {
+                val turnedOn = p1.getBooleanExtra("state", false)
+                Toast.makeText(
+                    p0,
+                    "Airplane mode changed. Turned: $turnedOn",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
         }
+    }
+
+    companion object {
+
+        const val ACTION_CLICKED = "clicked"
+        const val EXTRA_COUNT = "count"
     }
 }
